dfc54493b1a193365b85044fd2f15592c0d183e8
skygreen-spec <evensantiagokasri@gmail.com>
2026-06-28 13:59:09 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "SUPPORT HUB",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 4,
       },
     ],
   },
@@ -91,7 +91,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
+      primary: "#800080",
       secondary: "#2F3136",
 
       // Standard status colors for success/error/warning/info messages.
